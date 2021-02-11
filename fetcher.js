01d759b38f6514d9360cd9615ae30cdb3bb96172
Sandra To <sandra.to.e@gmail.com>
2021-02-11 06:30:30 +0000

    readline module activated if file path already exists

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,12 @@
 const request = require('request');
 const fs = require('fs');
+const  readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 const args = process.argv.slice(2);
 const url = args[0];
@@ -8,13 +15,36 @@ const filePath = args[1];
 // request webpage info
 request(url, (error, response, body) => {
   let dataSize = (body.length);
+  // checks if file exists
+  if (fs.existsSync(filePath)) {
+    rl.question('The file name entered already exists. To overwrite the file, type in \'Y\' and press \'Enter\' ', (answer) => {
+      if (answer === 'y' || answer === 'Y') {
+        fs.writeFile(filePath, body, err => {
+          if (err) {
+            return console.log(err);
+          } else {
+            console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
+            rl.close();
+          }
+        });
+        
+      } else {
+        console.log('Closing fetcher node app. Good bye!');
+        rl.close();
+      }
+    });
+  } else {
   // write in file system
-  fs.writeFile(filePath, body, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
-  });
+    fs.writeFile(filePath, body, function(err) {
+      if (err) {
+        return console.log(err);
+      } else {
+        // if file exists
+        console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
+        rl.close();
+      }
+    });
+  }
 });
 
 // ROUND ABOUT WAY TO GET FILE SIZE (DATA VARIABLE)
