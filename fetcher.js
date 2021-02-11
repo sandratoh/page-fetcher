e02c18ec459e2601e8ec5b9394b170d826c202f7
Sandra To <sandra.to.e@gmail.com>
2021-02-11 07:02:57 +0000

    Installed is-valid-path package, and node app returns message if file path is invalid

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const fs = require('fs');
-const  readline = require('readline');
+const readline = require('readline');
+const isValid = require('is-valid-path');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,41 +13,48 @@ const args = process.argv.slice(2);
 const url = args[0];
 const filePath = args[1];
 
-// request webpage info
-request(url, (error, response, body) => {
-  let dataSize = (body.length);
-  // checks if file exists
-  if (fs.existsSync(filePath)) {
-    rl.question('The file name entered already exists. To overwrite the file, type in \'Y\' and press \'Enter\' ', (answer) => {
-      if (answer === 'y' || answer === 'Y') {
-        fs.writeFile(filePath, body, err => {
-          if (err) {
-            return console.log(err);
-          } else {
-            console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
-            rl.close();
-          }
-        });
-        
-      } else {
-        console.log('Closing fetcher node app. Good bye!');
-        rl.close();
-      }
-    });
-  } else {
-  // write in file system
-    fs.writeFile(filePath, body, function(err) {
-      if (err) {
-        return console.log(err);
-      } else {
-        // if file exists
-        console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
-        rl.close();
-      }
-    });
-  }
-});
+// check valid file path
+if (!isValid(filePath)) {
+  rl.close();
+  return console.log('The file path is invalid. Please try again.');
+} else {
+  
+  // request webpage info
+  request(url, (error, response, body) => {
+    let dataSize = (body.length);
 
+    if (fs.existsSync(filePath)) {
+      rl.question('The file name entered already exists. To overwrite the file, type in \'Y\' and press \'Enter\' ', (answer) => {
+        if (answer === 'y' || answer === 'Y') {
+          fs.writeFile(filePath, body, err => {
+            if (err) {
+              return console.log(err);
+            } else {
+              console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
+              rl.close();
+            }
+          });
+        
+        } else {
+          console.log('Closing fetcher node app. Good bye!');
+          rl.close();
+        }
+      });
+
+    } else {
+      // write in file system
+      fs.writeFile(filePath, body, function(err) {
+        if (err) {
+          return console.log(err);
+        } else {
+        // if file exists
+          console.log(`Downloaded and saved ${dataSize} bytes to ${filePath}`);
+          rl.close();
+        }
+      });
+    }
+  });
+}
 // ROUND ABOUT WAY TO GET FILE SIZE (DATA VARIABLE)
 // request(url, (error, response, body) => {})
 //   .on('response', function(response) {
